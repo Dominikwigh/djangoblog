0632b9c0fd6b934744c9964bced791df71af1f41
Dominikwigh <dominik-00@hotmail.com>
2023-01-27 16:38:37 +0000

    change allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog1.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['django-blog1.herokuapp.com', 'localhost']
 
 
 # Application definition
